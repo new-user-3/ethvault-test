aa562dd32c3041fdc7d3f1ec6f8943ba7a0e1138
Piotr <piotr.inf@interia.pl>
2025-09-18 04:27:56 +0200

    add log message

--- backend/routes/smartContractFetchApitest.js
+++ backend/routes/smartContractFetchApitest.js
@@ -9,8 +9,8 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  const json = req.body;
-  console.log(json);
+  console.log("-------------------------- Received at: %s -----------------------------------", new Date().toISOString());
+  console.log(req.body);
   
   res.status(200).json({message: "Data taken correctly"});
 })
